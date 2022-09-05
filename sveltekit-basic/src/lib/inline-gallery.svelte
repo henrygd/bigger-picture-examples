049cd99489d814acbd96d87f10e0c6f490ea5729
hank <hank@henrygd.me>
2022-09-04 19:13:51 -0500

    finish sveltekit-basic example

---
+++ sveltekit-basic/src/lib/inline-gallery.svelte
@@ -0,0 +1,33 @@
+<script>
+	import BiggerPicture from 'bigger-picture/svelte';
+
+	// import style
+	import 'bigger-picture/css';
+
+	// image data
+	export let items = [];
+
+	// we can use directly in a use action
+	function openBpInline(target) {
+		const bp = BiggerPicture({
+			target,
+		});
+		bp.open({
+			items,
+			inline: true,
+			noClose: true,
+			scale: 1,
+			intro: 'fadeup',
+		});
+	}
+</script>
+
+<div use:openBpInline />
+
+<style>
+	div {
+		position: relative;
+		background: #444;
+		aspect-ratio: 3/2;
+	}
+</style>
